180ae17ca95ef52533830d492bfdbd9a2f0e9112
Omri Bendor <Omri.Bendor@kaltura.com>
2012-11-26 19:45:49 +0000

    def: 5948 | Replacing the usage of DOM document to KDOM.
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/tags/gemini_2012_10_22@90676 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- plugins/bulk_upload/xml/batch/BulkUploadEngineXml.php
+++ plugins/bulk_upload/xml/batch/BulkUploadEngineXml.php
@@ -116,7 +116,7 @@ class BulkUploadEngineXml extends KBulkUploadEngine
 		
 		$this->loadXslt();
 			
-		$xdoc = new DomDocument();
+		$xdoc = new KDOMDocument();
 		
 		$xmlContent = $this->xslTransform($this->data->filePath);
 		libxml_clear_errors();
