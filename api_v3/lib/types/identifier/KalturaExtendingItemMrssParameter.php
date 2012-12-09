49add2e8e69498ee9f5693d073f500f458e54bc1
Hila Karimov <Hila.Karimov@kaltura.com>
2012-12-09 17:29:15 +0000

    ps: new identifier classes
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/tags/gemini_2012_10_22@91250 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

---
+++ api_v3/lib/types/identifier/KalturaExtendingItemMrssParameter.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * @package api
+ * @subpackage objects
+ */
+class KalturaExtendingItemMrssParameter extends KalturaObject
+{
+	/**
+	 * XPath for the extending item
+	 * @var string
+	 */
+	public $xpath;
+	
+	/**
+	 * Object identifier
+	 * @var KalturaObjectIdentifier
+	 */
+	public $identifier;
+	
+	private static $map_between_objects = array(
+			"xpath",
+			"identifier",
+		);
+		
+	/* (non-PHPdoc)
+	 * @see KalturaObject::getMapBetweenObjects()
+	 */
+	public function getMapBetweenObjects()
+	{
+		return array_merge(parent::getMapBetweenObjects(), self::$map_between_objects);
+	}
+	
+	/* (non-PHPdoc)
+	 * @see KalturaObject::toObject($object_to_fill, $props_to_skip)
+	 */
+	public function toObject($dbObject = null, $propsToSkip = null)
+	{
+		if (!$dbObject)
+			$dbObject = new kExtendingItemMrssParameter();
+			
+		return parent::toObject($dbObject, $propsToSkip);
+	}
+	
+	/* (non-PHPdoc)
+	 * @see KalturaObject::fromObject($source_object)
+	 */
+	public function fromObject ($dbObject)
+	{
+		parent::fromObject($dbObject);
+		
+		/* @var $dbObject kExtendingItemMrssParameter */
+		$identifierType = get_class($dbObject->getIdentifier());
+		KalturaLog::info("Creating identifier for DB identifier type $identifierType");
+		switch ($identifierType)
+		{
+			case 'kEntryIdentifier':
+				$this->identifier = new KalturaEntryIdentifier();
+				break;
+			case 'kCategoryIdentifier':
+				$this->identifier = new KalturaCategoryIdentifier();
+		}
+		
+		$this->identifier->fromObject($dbObject->getIdentifier());
+		
+	}
+}
