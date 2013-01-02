64b31647893d8fadcf55bbfc2e409437f90953fe
Hila Karimov <Hila.Karimov@kaltura.com>
2013-01-02 17:20:14 +0000

    def: 6498
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/tags/gemini_2012_10_22@92460 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- plugins/bulk_upload/base/services/BulkService.php
+++ plugins/bulk_upload/base/services/BulkService.php
@@ -319,7 +319,7 @@ class BulkService extends KalturaBaseService
 		$c->addAnd(BatchJobLogPeer::JOB_ID, $id);
 		$c->addAnd(BatchJobLogPeer::PARTNER_ID, $this->getPartnerId());
 		$c->addAnd(BatchJobLogPeer::JOB_TYPE, BatchJobType::BULKUPLOAD);
-		$batchJob = BatchJobPeer::doSelectOne($c);
+		$batchJob = BatchJobLogPeer::doSelectOne($c);
 		
 		if (!$batchJob)
 			throw new KalturaAPIException(KalturaErrors::BULK_UPLOAD_NOT_FOUND, $id);
