a31d524fa6e4f74c3eff7a17023dcc77e4196fd0
Omri Bendor <Omri.Bendor@kaltura.com>
2013-01-03 13:37:20 +0000

    fwr: Added searching for partner via entryId, Remembering last partner search string within session bounderies and removing duplicate code.
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/backend/server/tags/gemini_2012_10_22@92538 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- admin_console/controllers/PartnerController.php
+++ admin_console/controllers/PartnerController.php
@@ -491,6 +491,19 @@ class PartnerController extends Zend_Controller_Action
 		$filterIncludRemoved = $request->getParam('include_removed');
 		$filterPackage = $request->getParam('partner_package');
 		
+		if (!in_array($filterType,array('','none'))) {
+			session_start();
+			$_SESSION['partnerLastSearchValue'] = $filterInput;
+		}
+		
+		if($filterType == 'byEntryId') {
+			$client = Infra_ClientHelper::getClient();
+			$adminConsolePlugin = Kaltura_Client_AdminConsole_Plugin::get($client);
+			/* @var $entry Kaltura_Client_Type_MediaEntry */
+			$entry = $adminConsolePlugin->entryAdmin->get($filterInput);
+			$filterType = 'byid';
+			$filterInput = $entry->partnerId;
+		}	
 		if ($filterType == 'byid')
 		{
 			$filter->idIn = $filterInput;
